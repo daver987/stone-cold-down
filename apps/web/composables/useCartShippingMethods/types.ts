4a21b3e5f79dab0e37caa1ad0c8befc085ba2633
daver987 <daveyalexander@outlook.com>
2023-11-19 05:03:40 -0500

    Refactor import statements for type clarity
    The import statements in the 'useCartShippingMethods/types.ts' and 'useProducts/useProducts.ts' files have been refactored to make use of type-only imports. This refining of the codebase ensures that the modules and types only generate type information and do not create any unnecessary runtime code. The code becomes easier to read and manage, growing in efficiency as a result of this update.

--- apps/web/composables/useCartShippingMethods/types.ts
+++ apps/web/composables/useCartShippingMethods/types.ts
@@ -1,4 +1,7 @@
-import type { SfShippingMethods, Maybe } from '@vue-storefront/unified-data-model'
+import type {
+  SfShippingMethods,
+  Maybe,
+} from '@vue-storefront/unified-data-model'
 
 export interface UseCartShippingMethodsState {
   data: Maybe<SfShippingMethods>
--- apps/web/composables/useProducts/useProducts.ts
+++ apps/web/composables/useProducts/useProducts.ts
@@ -1,4 +1,4 @@
-import {
+import type {
   FetchProducts,
   UseProductsReturn,
   UseProductsState,
